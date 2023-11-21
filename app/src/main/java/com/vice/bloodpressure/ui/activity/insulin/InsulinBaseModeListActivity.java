1d6b4ed121802769fbd52615c8e6c06ab3e3761a
username <email>
2023-11-21 14:01:10 +0800

    2021 03 31 13 22

--- app/src/main/java/com/vice/bloodpressure/ui/activity/insulin/InsulinBaseModeListActivity.java
+++ app/src/main/java/com/vice/bloodpressure/ui/activity/insulin/InsulinBaseModeListActivity.java
@@ -2,6 +2,7 @@ package com.vice.bloodpressure.ui.activity.insulin;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,6 +22,17 @@ import java.util.ArrayList;
  */
 public class InsulinBaseModeListActivity extends XYSoftUIBaseActivity {
 
+    /**
+     * 头部
+     * */
+    private ImageView IvBack;
+    private TextView tvModeFirst;
+    private TextView tvModeSecond;
+
+
+    /**
+     * 布局
+     */
     private TextView tvDeviceManage;
     private TextView tvBaseMode;
     private TextView tvDayAll;
@@ -33,9 +45,18 @@ public class InsulinBaseModeListActivity extends XYSoftUIBaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        topViewManager().titleTextView().setText("输注记录");
+        topViewManager().topView().removeAllViews();
+        topViewManager().topView().addView(initTopView());
         containerView().addView(initView());
         initValues();
+    }
+
+    private View initTopView() {
+        View topView = View.inflate(getPageContext(), R.layout.include_base_mode_top, null);
+        IvBack = topView.findViewById(R.id.iv_base_mode_back);
+        tvModeFirst = topView.findViewById(R.id.tv_base_mode_first);
+        tvModeSecond = topView.findViewById(R.id.tv_base_mode_second);
+        return topView;
     }
 
     private void initValues() {
@@ -45,7 +66,7 @@ public class InsulinBaseModeListActivity extends XYSoftUIBaseActivity {
 
 
     private View initView() {
-        View view = View.inflate(getPageContext(), R.layout.activity_insulin_infusion_record, null);
+        View view = View.inflate(getPageContext(), R.layout.activity_insulin_base_mode, null);
         tvDeviceManage = view.findViewById(R.id.tv_infusion_device_manage);
         tvBaseMode = view.findViewById(R.id.tv_infusion_base_mode);
         tvDayAll = view.findViewById(R.id.tv_infusion_info_day_all);
